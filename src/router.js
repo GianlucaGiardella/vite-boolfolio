0f94cd2eee14bc187a87824a44d4a2aea994e75f
GianluGiarde <gianlugiarde@gmail.com>
2023-07-17 17:54:34 +0200

    add router

---
+++ src/router.js
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import AppHome from "./pages/AppHome.vue";
+import ProjectsIndex from "./pages/ProjectsIndex.vue";
+import ProjectsShow from "./pages/ProjectsShow.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: AppHome,
+    },
+    {
+      path: "/projects",
+      name: "projects.index",
+      component: ProjectsIndex,
+    },
+    {
+      path: "/projects/:slug",
+      name: "projects.show",
+      component: ProjectsShow,
+    },
+  ],
+});
+
+export { router };
