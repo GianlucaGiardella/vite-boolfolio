bd4d89a4ba15589d9fcbd8262aeafe3c1e8028fe
GianluGiarde <gianlugiarde@gmail.com>
2023-07-17 18:12:15 +0200

    add pages

---
+++ src/pages/ProjectsIndex.vue
@@ -0,0 +1,16 @@
+<script>
+import ProjectList from "../components/ProjectList.vue";
+
+export default {
+  components: {
+    ProjectList,
+  },
+};
+</script>
+
+<template>
+  <h1>Lista Progetti</h1>
+  <ProjectList />
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/pages/ProjectsShow.vue
@@ -0,0 +1,9 @@
+<script>
+export default {};
+</script>
+
+<template>
+  <h2>Mostra il progetto</h2>
+</template>
+
+<style lang="scss" scoped></style>
