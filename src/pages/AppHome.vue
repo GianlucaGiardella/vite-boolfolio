36daeff3752bf63bc3779236919b1a322dbbd297
GianluGiarde <gianlugiarde@gmail.com>
2023-07-24 19:16:35 +0200

    add raondom calls

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -10,40 +10,28 @@ export default {
     };
   },
   created() {
-    axios.get(this.store.baseUrl + "api/projects/random").then((response) => {
-      this.arrProjects = response.data.results;
-    });
+    axios
+      .get(this.store.baseUrl + "api/projects/random")
+      .then(response => {
+        this.arrProjects = response.data.results;
+      });
   },
 };
 </script>
 
 <template>
   <div class="row row-cols-3 g-0">
-    <div class="col" v-for="project in arrProjects" :key="project.id">
-      <router-link
-        class="d-block w-100 h-100"
-        :to="{ name: 'projects.show', params: { slug: project.slug } }"
-      >
-        <img
-          class="w-100 h-100"
-          :src="store.getImageUrl(project.image)"
-          :alt="project.title"
-        />
+    <div class="col g-2" v-for="project in arrProjects" :key="project.id">
+      <router-link class="d-block w-100" style="height: 200px;"
+        :to="{ name: 'projects.show', params: { slug: project.slug } }">
+        <img class="w-100 h-100 rounded" :src="store.getImageUrl(project.image)" :alt="project.title" />
       </router-link>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-.row {
-  height: 70vh;
-}
-
 .col {
   height: calc(100% / 3);
-}
-
-img {
-  object-fit: cover;
 }
 </style>
